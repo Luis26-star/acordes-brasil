2541eae626b0e970f063e3ed4e40596fa89b888d
Luis26-star <fraglu@yahoo.com.br>
2026-04-21 12:31:57 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
-import { sendOrQueue } from './modules/offline.js';
-
 /* =========================================================
-   OFFLINE REQUEST WRAPPER
+   OFFLINE MODULE – FINAL (SENIOR LEVEL)
 ========================================================= */
 
-async function sendOrQueue(url, body) {
+const DB_NAME = 'choir-db';
+const STORE_NAME = 'queue';
+
+export async function sendOrQueue(url, body) {
   if (navigator.onLine) {
     return fetch(url, {
       method: 'POST',
@@ -15,8 +16,8 @@ async function sendOrQueue(url, body) {
 
   const db = await openDB();
 
-  const tx = db.transaction('queue', 'readwrite');
-  const store = tx.objectStore('queue');
+  const tx = db.transaction(STORE_NAME, 'readwrite');
+  const store = tx.objectStore(STORE_NAME);
 
   await store.add({
     id: Date.now(),
@@ -32,125 +33,22 @@ async function sendOrQueue(url, body) {
 }
 
 
-/* reuse DB */
+/* =========================================================
+   DB INIT (ROBUST!)
+========================================================= */
 function openDB() {
   return new Promise((resolve, reject) => {
-    const req = indexedDB.open('choir-db', 1);
+    const req = indexedDB.open(DB_NAME, 1);
+
+    req.onupgradeneeded = () => {
+      const db = req.result;
+
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id' });
+      }
+    };
+
     req.onsuccess = () => resolve(req.result);
     req.onerror = () => reject(req.error);
   });
 }
-/* =========================================================
-   APP.JS – FINAL SENIOR VERSION (DROP-IN READY)
-========================================================= */
-
-/* =========================================================
-   CONFIG
-========================================================= */
-const CONFIG = {
-  DEFAULT_LANGUAGE: 'de',
-  STORAGE_KEY: 'choir-app',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: ''
-};
-
-const isSupabaseConfigured = () =>
-  CONFIG.SUPABASE_URL && CONFIG.SUPABASE_ANON_KEY;
-
-
-/* =========================================================
-   UTILS
-========================================================= */
-const qs = (s) => document.querySelector(s);
-
-const safeHTML = (str) =>
-  String(str).replace(/[&<>"']/g, (m) => ({
-    "&": "&amp;", "<": "&lt;", ">": "&gt;",
-    '"': "&quot;", "'": "&#039;"
-  }[m]));
-
-
-/* =========================================================
-   EVENT BUS
-========================================================= */
-class EventBus {
-  constructor() {
-    this.events = {};
-  }
-
-  on(event, cb) {
-    (this.events[event] ||= []).push(cb);
-  }
-
-  emit(event, data) {
-    (this.events[event] || []).forEach(cb => cb(data));
-  }
-}
-
-
-/* =========================================================
-   STORE
-========================================================= */
-class Store {
-  constructor(initial) {
-    this.state = initial;
-    this.listeners = [];
-  }
-
-  get() {
-    return this.state;
-  }
-
-  set(patch) {
-    this.state = { ...this.state, ...patch };
-    this.listeners.forEach(l => l(this.state));
-  }
-
-  subscribe(fn) {
-    this.listeners.push(fn);
-  }
-}
-
-
-/* =========================================================
-   STORAGE
-========================================================= */
-class Storage {
-  load() {
-    try {
-      return JSON.parse(localStorage.getItem(CONFIG.STORAGE_KEY)) || {};
-    } catch {
-      return {};
-    }
-  }
-
-  save(data) {
-    localStorage.setItem(CONFIG.STORAGE_KEY, JSON.stringify(data));
-  }
-}
-
-
-/* =========================================================
-   ROUTER
-========================================================= */
-class Router {
-  constructor(app) {
-    this.app = app;
-    this.routes = {};
-  }
-
-  register(route, handler) {
-    this.routes[route] = handler;
-  }
-
-  go(route) {
-    location.hash = route;
-  }
-
-  resolve() {
-    const route = location.hash.replace('#', '') || 'home';
-    this.routes[route]?.();
-  }
-
-  init() {
-    window.addEventListener('hash
